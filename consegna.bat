42a4a40f184730a55f9d722190d74e741dfa014a
Andrea Maria <115480700+andream-castronovo@users.noreply.github.com>
2023-10-11 16:58:55 +0200

    Rimozione .git
    Il prof ha problemi col programma e non gli piace più .git nella consegna

--- consegna.bat
+++ consegna.bat
@@ -2,14 +2,14 @@
 
 set intro=false
 if %intro% EQU true (
-echo Cambia le prime righe del codice per impostare i percorsi vari
-echo dopo elimina queste 4 righe di codice 
-echo ^(Tasto destro, apri con, notepad^)
-echo.
-echo Se vuoi che questo programma faccia anche il commit con git in caso ti dimentichi, metti la variabile "git=true"
-echo.
-pause
-goto :EOF
+    echo Cambia le prime righe del codice per impostare i percorsi vari
+    echo dopo elimina queste 4 righe di codice 
+    echo ^(Tasto destro, apri con, notepad^)
+    echo.
+    echo Se vuoi che questo programma faccia anche il commit con git in caso ti dimentichi, metti la variabile "git=true"
+    echo.
+    pause
+    goto :EOF
 )
 
 REM Variabili da cambiare
@@ -22,11 +22,10 @@ set data=%date:~6%-%date:~3,2%-%date:~0,2%
 
 REM Chiedi il percorso
 :promptPercorso
-set /p percorso=Inserisci il percorso da consegnare senza virgolette anche se ci sono spazi --^> 
+set /p percorso=Inserisci il percorso da consegnare --^> 
 if not exist "%percorso%" ( echo Il percorso non esiste & goto :promptPercorso )
 
-
-REM Chiedi che materia Ã¨ da consegnare
+REM Chiedi che materia è da consegnare
 :promptMateria
 set /p materia=tps o info^? --^> 
 if not %materia% EQU tps (
@@ -40,27 +39,15 @@ cd /D "%percorso%"
 
 REM Elimina tutte le cartelle bin/ obj/ e .vs/ ed il loro contenuto
 :binobjvs
-
-REM Controlla che Visual Studio sia chiudo
-:controlloVS
-
-tasklist /fi "imagename eq devenv.exe" | find ":" > nul
-if errorlevel 1 echo Chiudi VisualStudio. & pause & goto :controlloVS
-
-REM Cancella le cartelle bin, obj e .vs
 FOR /d /r . %%d IN (bin) DO @IF EXIST "%%d" rd /s /q "%%d"
 FOR /d /r . %%d IN (obj) DO @IF EXIST "%%d" rd /s /q "%%d"
 FOR /d /r . %%d IN (.vs) DO @IF EXIST "%%d" rd /s /q "%%d"
 
-REM Effettua l'add, il commit e il push se Ã¨ stato dimenticato
+REM Effettua l'add, il commit e il push se è stato dimenticato
 :git 
-if %git% EQU true ( echo. & echo ^> Git Add ^< & echo. & git add . & echo. & echo ^> Git Commit ^< & echo. & git commit -m "Commit automatico consegna %data%" & echo. & echo ^> Git push ^< & echo. & git push & echo.)
+if %git% EQU true ( git add . & git commit -m "Commit automatico consegna %data%" & git push )
 
 REM Crea la cartella con il formato di data che vuole il prof
-
-echo ^> Creo la cartella con il formato di data che vuole il prof ^<
-echo.
-
 if not %materia% EQU tps ( goto :info )
 
 if exist %TecnProg% ( cd /D %TecnProg% ) else ( echo Cartella TecnProg inesistente ^(Prova a cambiare il percorso in questo file^) & pause & goto :EOF)
@@ -83,13 +70,15 @@ cd /D %Informatica%\%data%
 :copyFolder
 REM Per ottenere il nome della cartella dal percorso
 for %%i IN ("%percorso%") DO (set filename=%%~ni)
+
 mkdir "%filename%"
 cd %filename%
-
-echo ^> Copio il contenuto di %percorso% in %cd% ^<
-echo.
-
 xcopy /E /H /Y "%percorso%" .\
 
+REM Ripeto il processo di eliminazione in caso qualcosa non è stato cancellato correttamente.
+FOR /d /r . %%d IN (bin) DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /d /r . %%d IN (obj) DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /d /r . %%d IN (.vs) DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /d /r . %%d IN (.git) DO @IF EXIST "%%d" rd /s /q "%%d"
+
 explorer ..\
-pause
